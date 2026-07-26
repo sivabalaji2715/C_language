0385d2978672c589b93019017ac2b74c0955c765
sivabalaji2715 <sakthirathi77@gmail.com>
2026-07-26 13:56:04 +0530

    Create assingment3 count_vowles_space_constant_digits.c

---
+++ assingment3 count_vowles_space_constant_digits.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i;
+    int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+
+    // Input string
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Count vowels, consonants, digits, and spaces
+    for (i = 0; str[i] != '\0'; i++) {
+        if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')) {
+            if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' ||
+                str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        } else if (str[i] >= '0' && str[i] <= '9') {
+            digits++;
+        } else if (str[i] == ' ') {
+            spaces++;
+        }
+    }
+
+    // Display results
+    printf("Vowels = %d\n", vowels);
+    printf("Consonants = %d\n", consonants);
+    printf("Digits = %d\n", digits);
+    printf("Spaces = %d\n", spaces);
+
+    return 0;
+}
