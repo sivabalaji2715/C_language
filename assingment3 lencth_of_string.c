ec67dda258b51f5efebee89dff3303eab422cc6d
sivabalaji2715 <sakthirathi77@gmail.com>
2026-07-26 13:45:55 +0530

    Create assingment3 lencth_of_string.c

---
+++ assingment3 lencth_of_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int stringLength(char str[]) {
+    int i = 0;
+
+    while (str[i] != '\0') {
+        i++;
+    }
+
+    return i;
+}
+
+int main() {
+    char str[100];
+
+    // Input string
+    printf("Enter a string: ");
+    scanf("%99s", str);
+
+    // Find and print length
+    printf("Length of the string = %d\n", stringLength(str));
+
+    return 0;
+}
