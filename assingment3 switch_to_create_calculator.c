454a0196d1b1c7142ad6cfb04f013e9b644004b6
sivabalaji2715 <sakthirathi77@gmail.com>
2026-07-26 13:12:13 +0530

    Create assingment3 switch_to_create_calculator.c

---
+++ assingment3 switch_to_create_calculator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2;
+    char op;
+
+    // Input
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    // Perform calculation
+    switch (op) {
+        case '+':
+            printf("Result = %.2f\n", num1 + num2);
+            break;
+
+        case '-':
+            printf("Result = %.2f\n", num1 - num2);
+            break;
+
+        case '*':
+            printf("Result = %.2f\n", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                printf("Result = %.2f\n", num1 / num2);
+            else
+                printf("Error! Division by zero is not allowed.\n");
+            break;
+
+        default:
+            printf("Invalid operator.\n");
+    }
+
+    return 0;
+}
