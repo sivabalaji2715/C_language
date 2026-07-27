607083c0eaa3d8433c72c9146aa08233629b8250
sivabalaji2715 <sakthirathi77@gmail.com>
2026-07-27 15:54:28 +0530

    Update assingment3 count_digit_in_number.c

--- assingment3 count_digit_in_number.c
+++ assingment3 count_digit_in_number.c
@@ -3,17 +3,17 @@
 int main() {
     int num, count = 0;
 
-    // Input
+    
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Count digits
+    
     while (num != 0) {
         num = num / 10;
         count++;
     }
 
-    // Output
+    
     printf("Number of digits = %d\n", count);
 
     return 0;
