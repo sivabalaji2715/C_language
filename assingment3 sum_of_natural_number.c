e931cca6813068c200f5096940dcf3c19272e61e
sivabalaji2715 <sakthirathi77@gmail.com>
2026-07-28 15:39:52 +0530

    Update assingment3 sum_of_natural_number.c

--- assingment3 sum_of_natural_number.c
+++ assingment3 sum_of_natural_number.c
@@ -3,16 +3,16 @@
 int main() {
     int n, i, sum = 0;
 
-    // Input
+    
     printf("Enter the value of N: ");
     scanf("%d", &n);
 
-    // Calculate sum
+    
     for (i = 1; i <= n; i++) {
         sum = sum + i;
     }
 
-    // Display result
+    
     printf("Sum of first %d natural numbers = %d\n", n, sum);
 
     return 0;
