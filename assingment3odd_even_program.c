3e95716726a9268016b6a5b277b0fb6d5b5ecbf2
sivabalaji2715 <sakthirathi77@gmail.com>
2026-07-28 15:36:09 +0530

    Update assingment3odd_even_program.c

--- assingment3odd_even_program.c
+++ assingment3odd_even_program.c
@@ -3,11 +3,11 @@
 int main() {
     int num;
 
-    // Input
+    
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Check even or odd
+    
     if (num % 2 == 0) {
         printf("%d is an Even number.\n", num);
     } else {
