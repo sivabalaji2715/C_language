c1b72b88fc5704055d9aec1fab67a1f0af222c4c
sivabalaji2715 <sakthirathi77@gmail.com>
2026-07-28 15:43:27 +0530

    Update assingment3 reverse_an_array.c

--- assingment3 reverse_an_array.c
+++ assingment3 reverse_an_array.c
@@ -3,13 +3,13 @@
 int main() {
     int arr[4], i;
 
-    // Input 4 elements
+    
     printf("Enter 4 integers:\n");
     for (i = 0; i < 4; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Print array in reverse order
+
     printf("Reversed array: ");
     for (i = 3; i >= 0; i--) {
         printf("%d ", arr[i]);
