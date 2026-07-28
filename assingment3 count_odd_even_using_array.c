bcce4fd78504bc01e05774c7b9b5f56fe0d977aa
sivabalaji2715 <sakthirathi77@gmail.com>
2026-07-28 15:38:04 +0530

    Update assingment3 count_odd_even_using_array.c

--- assingment3 count_odd_even_using_array.c
+++ assingment3 count_odd_even_using_array.c
@@ -4,13 +4,13 @@ int main() {
     int arr[10], i;
     int even = 0, odd = 0;
 
-    // Input 10 integers
+    
     printf("Enter 10 integers:\n");
     for (i = 0; i < 10; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Count even and odd numbers
+    
     for (i = 0; i < 10; i++) {
         if (arr[i] % 2 == 0)
             even++;
@@ -18,7 +18,7 @@ int main() {
             odd++;
     }
 
-    // Display result
+
     printf("Number of Even elements = %d\n", even);
     printf("Number of Odd elements = %d\n", odd);
 
