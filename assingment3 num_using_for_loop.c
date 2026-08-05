e8cd82b22014c5f6a85052e09015f944f016a7c0
sivabalaji2715 <sakthirathi77@gmail.com>
2026-08-05 16:23:44 +0530

    Update assingment3 num_using_for_loop.c

--- assingment3 num_using_for_loop.c
+++ assingment3 num_using_for_loop.c
@@ -3,11 +3,11 @@
 int main() {
     int n, i;
 
-    // Input
+    
     printf("Enter the value of N: ");
     scanf("%d", &n);
 
-    // Print numbers from 1 to N
+    
     for (i = 1; i <= n; i++) {
         printf("%d ", i);
     }
